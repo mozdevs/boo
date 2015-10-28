63fdc472c0d914e9c07799866a8def7eff83178f
Belén Albeza <belen.albeza@gmail.com>
2015-10-28 16:30:58 +0100

    move Boo to a module

---
+++ app/js/boo.js
@@ -0,0 +1,62 @@
+function Boo(stream, original, filtered) {
+    this.stream = stream;
+    this.video = original;
+    this.canvas = filtered;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.video.src = window.URL.createObjectURL(stream);
+    this.video.play();
+
+    this.video.addEventListener('loadeddata', function () {
+        window.requestAnimationFrame(this._tick.bind(this));
+        this.canvasStream = this.canvas.captureStream(12);
+        this.canvasStream.addTrack(this.stream.getAudioTracks()[0]);
+        this.recorder = new MediaRecorder(this.canvasStream);
+        this.recorder.start();
+    }.bind(this), false);
+}
+
+Boo.prototype._tick = function () {
+    window.requestAnimationFrame(this._tick.bind(this));
+    this.render();
+};
+
+Boo.prototype.render = function () {
+    var width, height, offsetX, offsetY;
+    var ratio = this.video.videoWidth / this.video.videoHeight;
+
+    if (ratio > 0) { // landscape
+        height = this.canvas.height;
+        width = ratio * this.canvas.width;
+        offsetX = (this.canvas.width - width) / 2;
+        offsetY = 0;
+    }
+    else { // portrait
+        width = this.canvas.width;
+        height = ratio * this.canvas.height;
+        offsetY = (this.canvas.height - height) / 2;
+        offsetX = 0;
+    }
+
+    this.ctx.drawImage(
+        this.video,
+        0, 0,
+        this.video.videoWidth, this.video.videoHeight,
+        offsetX, offsetY,
+        width, height);
+};
+
+Boo.prototype.download = function () {
+    this.recorder.stop();
+    this.recorder.ondataavailable = function (evt) {
+        var link = document.createElement('a');
+        link.setAttribute('href', window.URL.createObjectURL(evt.data));
+        link.setAttribute('download', 'video.webm');
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+};
+
+module.exports = Boo;
