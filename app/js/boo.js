f74cb89ae75eecb1f077ac6a177e77f6ccd9c5a6
Belén Albeza <belen.albeza@gmail.com>
2015-10-28 15:27:28 +0100

    changed project file structure

--- app/js/boo.js
+++ app/js/boo.js
@@ -7,12 +7,14 @@ function Boo(stream, original, filtered) {
     this.video.src = window.URL.createObjectURL(stream);
     this.video.play();
 
+    this.renderer = new PIXI.WebGLRenderer(
+        this.canvas.width, this.canvas.height, { view: this.canvas });
 
     this.video.addEventListener('loadeddata', function () {
         window.requestAnimationFrame(this._tick.bind(this));
         this.canvasStream = this.canvas.captureStream(12);
         this.canvasStream.addTrack(this.stream.getAudioTracks()[0]);
-        this.recorder = new MediaRecorder(this.canvasStream, { mimeType: 'video/mp4' });
+        this.recorder = new MediaRecorder(this.canvasStream);
         this.recorder.start();
     }.bind(this), false);
 }
