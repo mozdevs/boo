78162f64684e5c63aa027c212e15cb22ff3ce80a
Belén Albeza <belen.albeza@gmail.com>
2015-10-28 16:16:29 +0100

    testing page deploy

---
+++ js/bundle.js
@@ -0,0 +1,89 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function Boo(stream, original, filtered) {
+    this.stream = stream;
+    this.video = original;
+    this.canvas = filtered;
+    this.ctx = this.canvas.getContext('2d');
+
+    this.video.src = window.URL.createObjectURL(stream);
+    this.video.play();
+
+    this.video.addEventListener('loadeddata', function () {
+        window.requestAnimationFrame(this._tick.bind(this));
+        this.canvasStream = this.canvas.captureStream(12);
+        this.canvasStream.addTrack(this.stream.getAudioTracks()[0]);
+        this.recorder = new MediaRecorder(this.canvasStream);
+        this.recorder.start();
+    }.bind(this), false);
+}
+
+Boo.prototype._tick = function () {
+    window.requestAnimationFrame(this._tick.bind(this));
+    this.render();
+};
+
+Boo.prototype.render = function () {
+    var width, height, offsetX, offsetY;
+    var ratio = this.video.videoWidth / this.video.videoHeight;
+
+    if (ratio > 0) { // landscape
+        height = this.canvas.height;
+        width = ratio * this.canvas.width;
+        offsetX = (this.canvas.width - width) / 2;
+        offsetY = 0;
+    }
+    else { // portrait
+        width = this.canvas.width;
+        height = ratio * this.canvas.height;
+        offsetY = (this.canvas.height - height) / 2;
+        offsetX = 0;
+    }
+
+    this.ctx.drawImage(
+        this.video,
+        0, 0,
+        this.video.videoWidth, this.video.videoHeight,
+        offsetX, offsetY,
+        width, height);
+};
+
+Boo.prototype.download = function () {
+    this.recorder.stop();
+    this.recorder.ondataavailable = function (evt) {
+        var link = document.createElement('a');
+        link.setAttribute('href', window.URL.createObjectURL(evt.data));
+        link.setAttribute('download', 'video.webm');
+        link.style.display = 'none';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+};
+
+window.onload = function () {
+    var boo;
+
+    navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true
+    }).then(function (stream) {
+        var video = document.getElementById('booth-original');
+        var boo = new Boo(
+            stream,
+            video,
+            document.getElementById('booth-filtered')
+        );
+
+        var button = document.getElementById('download');
+        video.addEventListener('loadeddata', function () {
+            button.disabled = false;
+        });
+        button.addEventListener('click', function () {
+            button.disabled = true;
+            boo.download();
+        });
+
+    });
+};
+
+},{}]},{},[1]);
