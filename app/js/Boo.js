cb4d5f69467817013213d9488015011ec18ed07b
sole <sole@mozilla.com>
2015-11-11 09:48:26 +0000

    rewrite to use scope variables instead of prototype based objects, fixes issue #17

--- app/js/Boo.js
+++ app/js/Boo.js
@@ -1,120 +1,108 @@
 var Renderer = require('./Renderer.js');
-var EventEmitter = require('events');
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
 
 function errorHandler(msg) {
     throw new Error(msg);
 }
 
 function Boo(stream, originalCanvas, filteredCanvas, progressBar) {
-    // inherit from EventEmitter
+
+    var self = this;
+    var video;
+    var originalRenderer, filteredRenderer;
+    var canvasStream;
+    var recorder;
+    var RECORD_TIME = 6; // in seconds
+    var isRecording = false;
+    var recordingStartTime;
+
     EventEmitter.call(this);
 
-    this.progressBar = progressBar;
-    this.originalRenderer = new Renderer(originalCanvas, errorHandler, null);
+    originalRenderer = new Renderer(originalCanvas, errorHandler, null);
+    filteredRenderer = new Renderer(filteredCanvas, errorHandler, onStreamReady);
 
-    this.filteredRenderer = new Renderer(filteredCanvas, errorHandler, function onStreamReady() {
+    function onStreamReady() {
         // create a muted, invisible video element to stream 
         // the camera/mic output to
-        this.video = document.createElement('video');
-        this.video.style = 'display:none';
-        this.video.muted = true;
-        this.video.src = window.URL.createObjectURL(stream);
-        document.body.appendChild(this.video);
-        this.video.play();
+        video = document.createElement('video');
+        video.style = 'display:none';
+        video.muted = true;
+        video.src = window.URL.createObjectURL(stream);
+        document.body.appendChild(video);
+        video.play();
 
-        this.video.addEventListener('loadedmetadata', function () {
-            var w = this.video.videoWidth;
-            var h = this.video.videoHeight;
-            this.filteredRenderer.setSize(w, h);
-            this.originalRenderer.setSize(w, h);
-        }.bind(this));
+        video.addEventListener('loadedmetadata', function () {
+            var w = video.videoWidth;
+            var h = video.videoHeight;
+            filteredRenderer.setSize(w, h);
+            originalRenderer.setSize(w, h);
+        });
 
-        this.filteredRenderer.nextEffect();
+        filteredRenderer.nextEffect();
 
-        this.video.addEventListener('loadeddata', function () {
-            this.emit('ready');
-            window.requestAnimationFrame(this._tick.bind(this));
-            this.canvasStream = filteredCanvas.captureStream(12);
-            this.canvasStream.addTrack(this.stream.getAudioTracks()[0]);
-            this.recorder = new MediaRecorder(this.canvasStream);
-        }.bind(this), false);
-    }.bind(this));
-
-    this.stream = stream;
-}
-
-// inherit from EventEmitter
-Boo.prototype = Object.create(EventEmitter.prototype);
-Boo.prototype.constructor = Boo;
-
-Boo.RECORD_TIME = 6; // in seconds
-
-Boo.prototype._tick = function (elapsed) {
-    if (this.isRecording) { // update progress bar when recording
-        if (!this._started) {
-            this._started = elapsed;
-        }
-        else {
-            this.progressBar.value = (elapsed - this._started) / 1000;
-        }
+        video.addEventListener('loadeddata', function () {
+            self.emit('ready');
+            requestAnimationFrame(animate);
+            canvasStream = filteredCanvas.captureStream(12);
+            canvasStream.addTrack(stream.getAudioTracks()[0]);
+            recorder = new MediaRecorder(canvasStream);
+        });
     }
 
-    window.requestAnimationFrame(this._tick.bind(this));
-    this.originalRenderer.updateTexture(this.video);
-    this.filteredRenderer.updateTexture(this.video);
-};
 
-Boo.prototype.record = function () {
-    this.recorder.start();
-    this._started = null;
+    function animate(elapsedTime) {
 
-    this.progressBar.max = Boo.RECORD_TIME;
-    this.progressBar.value = 0;
+        requestAnimationFrame(animate);
 
-    this.isRecording = true;
+        if (isRecording) { // update progress bar when recording
+            if (!recordingStartTime) {
+                recordingStartTime = elapsedTime;
+            } else {
+                progressBar.value = (elapsedTime - recordingStartTime) / 1000;
+            }
+        }
 
-    this.recorder.ondataavailable = function (evt) {
-        this.emit('finished', evt.data);
-    }.bind(this);
+        originalRenderer.updateTexture(video);
+        filteredRenderer.updateTexture(video);
+    }
 
-    setTimeout(function () {
-        this.isRecording = false;
-        this.recorder.stop();
-        this.progressBar.value = this.progressBar.max;
-    }.bind(this), Boo.RECORD_TIME * 1000);
-};
 
-Boo.prototype.getVideoEffects = function ()  {
-    return this.filteredRenderer.getEffects();
-};
+    function onRecordingTimeOver() {
+        isRecording = false;
+        recorder.stop();
+        progressBar.value = progressBar.max;
+    }
 
-Boo.prototype.applyVideoEffect = function (index) {
-    this.filteredRenderer.selectEffect(index);
-};
 
-// Boo.prototype.render = function () {
-//     var width, height, offsetX, offsetY;
-//     var ratio = this.video.videoWidth / this.video.videoHeight;
-//
-//     if (ratio > 0) { // landscape
-//         height = this.original.height;
-//         width = ratio * this.original.width;
-//         offsetX = (this.original.width - width) / 2;
-//         offsetY = 0;
-//     }
-//     else { // portrait
-//         width = this.original.width;
-//         height = ratio * this.original.height;
-//         offsetY = (this.original.height - height) / 2;
-//         offsetX = 0;
-//     }
-//
-//     this.ctx.drawImage(
-//         this.video,
-//         0, 0,
-//         this.video.videoWidth, this.video.videoHeight,
-//         offsetX, offsetY,
-//         width, height);
-// };
+    this.record = function () {
+        recorder.start();
+        recordingStartTime = null;
+
+        progressBar.max = RECORD_TIME;
+        progressBar.value = 0;
+
+        isRecording = true;
+
+        recorder.ondataavailable = function (evt) {
+            self.emit('finished', evt.data);
+        };
+
+        setTimeout(onRecordingTimeOver, RECORD_TIME * 1000);
+    };
+
+
+    this.getVideoEffects = function ()  {
+        return filteredRenderer.getEffects();
+    };
+
+
+    this.applyVideoEffect = function (index) {
+        filteredRenderer.selectEffect(index);
+    };
+
+}
+
+util.inherits(Boo, EventEmitter);
 
 module.exports = Boo;
