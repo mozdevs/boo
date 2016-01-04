0a2f1b98885a1aedc3c2bedf8d15533fd365ab53
sole <sole@mozilla.com>
2016-01-04 14:14:51 +0000

    Simplify inverted shader

--- app/shaders/inverted.fs
+++ app/shaders/inverted.fs
@@ -3,5 +3,5 @@ varying vec2 vUv;
 
 void main() {
     vec3 rgb = texture2D(map, vUv).rgb;
-    gl_FragColor = vec4(1.0 - rgb.r, 1.0 - rgb.g, 1.0 - rgb.b, 1.0);
+    gl_FragColor = vec4(1.0 - rgb, 1.0);
 }
