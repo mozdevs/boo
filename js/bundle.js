49bf8041e6d077609905ec0fb4ca198118582ded
sole <sole@mozilla.com>
2016-11-29 16:46:54 +1100

    Updates

--- js/bundle.js
+++ js/bundle.js
@@ -344,13 +344,13 @@ var Renderer = function(canvas, errorCallback, readyCallback) {
     var effects = [];
     var effectDefinitions = {
         'None': { vertex: 'plane.vs', fragment: 'none.fs' },
-        'HalfTone': { vertex: 'plane.vs', fragment: 'halftone.fs' },
 		'Witch': { vertex: 'plane.vs', fragment: 'greenmonster.fs' },
         'Lo-Fi Purple': { vertex: 'plane.vs', fragment: 'lofipurple.fs' },
         'Pixelated Red': { vertex: 'plane.vs', fragment: 'redpixel.fs' },
         'Burnt': { vertex: 'plane.vs', fragment: 'burnt.fs' },
-        'Inverted': { vertex: 'plane.vs', fragment: 'inverted.fs' }
-
+        'Inverted': { vertex: 'plane.vs', fragment: 'inverted.fs' },
+        'HalfTone': { vertex: 'plane.vs', fragment: 'halftone.fs' },
+        'Abstract Tone': { vertex: 'plane.vs', fragment: 'halftone-abstract.fs' }
     };
     var activeEffect = null;
     var shadersReady = false;
